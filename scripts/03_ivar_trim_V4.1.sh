0b23c4c3e92572b2fa2f195d27d6f57fd3a8380b
Beau Schaeffer <76972996+BeauSchaeffer@users.noreply.github.com>
2025-10-21 16:47:20 -0400

    add ivar environment

--- scripts/03_ivar_trim_V4.1.sh
+++ scripts/03_ivar_trim_V4.1.sh
@@ -10,7 +10,7 @@
 
 # load Conda and activate local environment
 source ~/.bashrc
-conda activate ./env/ivar
+conda activate /n/holylfs05/LABS/hanage_lab/Lab/hsphfs1/bschaeffer/envs/ivar
 
 # generate sample ID from sample.txt and array index
 sample=$(sed -n "$((SLURM_ARRAY_TASK_ID + 1))p" samples.txt)
