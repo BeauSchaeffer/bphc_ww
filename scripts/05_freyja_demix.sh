393ce311e07ffbc7e705bc91aae3e05d89b7bd5e
Beau Schaeffer <76972996+BeauSchaeffer@users.noreply.github.com>
2026-03-31 17:06:44 -0400

    array indices and mem request

--- scripts/05_freyja_demix.sh
+++ scripts/05_freyja_demix.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --time=00:20:00
 #SBATCH -p hsph
 #SBATCH --mem=4G
 #SBATCH --job-name=freyja_demix
 #SBATCH --output=logs/freyja_demix_%A_%a.out
 #SBATCH --error=logs/freyja_demix_%A_%a.err
-#SBATCH --array=0-315   # <-- Update to match number of samples in samples.txt ###** UPDATE **###
+#SBATCH --array=0-304   # <-- Update to match number of samples in samples.txt ###** UPDATE **###
 
 # load Conda and activate local environment
 source ~/.bashrc
@@ -28,6 +28,7 @@ echo "Running freyja demix for $sample..."
 # deconvolution with Freyja to recover lineage relative abundances
   # --eps 0.000001 = lower threshold for dropping low abundance lineages so that RAs sum to 1
   # --depthcutoff 10 = default 0, was using this to troubleshoot but have resolved with new version
+  # --autoadapt = can help a bit to deal with the high error rates with low quality samples
 freyja demix \
   "$variants" "$depths" \
   --output "$outfile" \
